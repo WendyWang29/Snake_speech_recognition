c9012672cccc9671e1efffde044835d58dad8fc0
WendyWang29 <82058944+WendyWang29@users.noreply.github.com>
2021-11-02 18:21:23 +0100

    Create sound.js
    created the kick() function

---
+++ sound.js
@@ -0,0 +1,15 @@
+export function kick() {
+    let cx = new AudioContext()
+    let o = cx.createOscillator()
+    let g = cx.createGain()
+    let now = cx.currentTime
+    //(frequency, start time)
+    o.frequency.setValueAtTime(100, now)
+    g.gain.setValueAtTime(0.5, now)
+    o.frequency.exponentialRampToValueAtTime(0.001, now+0.5)
+    g.gain.exponentialRampToValueAtTime(0.001, now+0.5)
+    o.connect(g)
+    g.connect(cx.destination)
+    o.start(now)
+    o.stop(now+0.5)
+}
